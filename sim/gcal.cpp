tindzk/SelfOrganisingMaps

commit 2a17d01be8a665fdb8f6608337a73eaa2c032bad

topo: Annotate homeostasis logic

sim/gcal.cpp
@@ -46,15 +46,17 @@ public:
     float sigmaA, sigmaB, afferRadius, excitRadius, inhibRadius, afferSigma, excitSigma, inhibSigma, LGNCenterSigma, LGNSurroundSigma;
 
     void init(Json::Value root) {
-        // GET PARAMS FROM JSON
-        homeostasis = root.get("homeostasis", true).asBool();
+        // Read parameters from JSON
         settle = root.get("settle", 16).asUInt();
 
         // homeostasis
+        homeostasis = root.get("homeostasis", true).asBool();
         beta = root.get("beta", 0.991).asFloat();
         lambda = root.get("lambda", 0.01).asFloat();
         mu = root.get("thetaInit", 0.15).asFloat();
         thetaInit = root.get("mu", 0.024).asFloat();
+
+        // Gaussian
         xRange = root.get("xRange", 2.0).asFloat();
         yRange = root.get("yRange", 2.0).asFloat();
 
@@ -118,12 +120,8 @@ public:
         for (auto &p: LGN_OFF.Projections) p.renormalize();
 
         // Cortex Sheet (V1)
-        CX.beta = beta;
-        CX.lambda = lambda;
-        CX.mu = mu;
-        CX.thetaInit = thetaInit;
         CX.svgpath = root.get("CX_svgpath", "boundaries/trialmod.svg").asString();
-        CX.init();
+        CX.init({beta = beta, lambda = lambda, mu = mu, thetaInit = thetaInit});
         CX.allocate();
 
         // afferent projection from ON/OFF cells
@@ -197,6 +195,8 @@ public:
      */
     void stepCortex(const std::function<void(HexGrid*, vector<double>&)> f) {
         CX.zero_X();
+        // From paper: "Once all 16 settling steps are complete, the settled V1 activation pattern is deemed to be the
+        // V1 response to the presented pattern."
         for (size_t j = 0; j < settle; j++) {
             CX.step();
             f(CX.hg, CX.X);

sim/topo.h
@@ -177,7 +177,7 @@ public:
     vector<Projection<Flt>> Projections;
     alignas(alignof(vector<Flt>)) vector<Flt> X;
     alignas(alignof(vector<Flt *>)) vector<Flt *> Xptr;
-    vector<vector<int> > P;            // identity of projections to (potentially) joint normalize
+    vector<vector<int>> P;            // identity of projections to (potentially) joint normalize
 
     virtual void init() {
         this->stepCount = 0;
@@ -302,17 +302,34 @@ public:
 };
 
 template<class Flt>
+struct HomeostasisParameters {
+    // Degree of smoothing in average calculation
+    Flt beta;
+
+    // Target V1 unit activity
+    Flt mu;
+
+    // Homeostatic learning rate
+    Flt lambda;
+
+    // Initial threshold
+    Flt thetaInit;
+};
+
+template<class Flt>
 class CortexSOM : public RD_Sheet<Flt> {
-public:
-    Flt beta, lambda, mu, oneMinusBeta, thetaInit;
+private:
+    HomeostasisParameters<Flt> params;
 
+    // Smoothed average activities
     alignas(alignof(vector<Flt>)) vector<Flt> Xavg;
-    alignas(alignof(vector<Flt>)) vector<Flt> Theta;
 
-    virtual void init() {
-        oneMinusBeta = (1. - beta);
-        this->stepCount = 0;
-        this->zero_vector_variable(this->X);
+    // Thresholds
+    alignas(alignof(vector<Flt>)) vector<Flt> Theta;
+public:
+    virtual void init(const HomeostasisParameters<Flt> params) {
+        RD_Sheet<Flt>::init();
+        this->params = params;
         this->zero_vector_variable(this->Xavg);
         this->zero_vector_variable(this->Theta);
     }
@@ -322,8 +339,8 @@ public:
         this->resize_vector_variable(this->Xavg);
         this->resize_vector_variable(this->Theta);
         for (size_t hi = 0; hi < this->nhex; ++hi) {
-            this->Xavg[hi] = mu;
-            this->Theta[hi] = thetaInit;
+            this->Xavg[hi] = params.mu;
+            this->Theta[hi] = params.thetaInit;
         }
     }
 
@@ -337,15 +354,22 @@ public:
         sheetStep(this->nhex, projections, this->Theta, this->fields, this->X);
     }
 
+    /**
+     * Perform homeostasis, i.e. calculate average activations and update thresholds
+     *
+     * From paper: "The effect of this scaling mechanism is to bring the average activity of each V1 unit closer to the
+     * specified target."
+     */
     void homeostasis() {
-#pragma omp parallel for
-        for (size_t hi = 0; hi < this->nhex; ++hi) {
-            this->Xavg[hi] = oneMinusBeta * this->X[hi] + beta * this->Xavg[hi];
-        }
-#pragma omp parallel for
-        for (size_t hi = 0; hi < this->nhex; ++hi) {
-            this->Theta[hi] = this->Theta[hi] + lambda * (this->Xavg[hi] - mu);
-        }
+        // Calculate average given degree of smoothing (beta) as per eq. 7
+#pragma omp parallel for default(none)
+        for (size_t hi = 0; hi < this->nhex; ++hi)
+            Xavg[hi] = (1. - params.beta) * this->X[hi] + params.beta * Xavg[hi];
+
+        // Update thresholds as per eq. 8
+#pragma omp parallel for default(none)
+        for (size_t hi = 0; hi < this->nhex; ++hi)
+            Theta[hi] = Theta[hi] + params.lambda * (Xavg[hi] - params.mu);
     }
 };
 

commit bc333c5eea29d3750205e05b8c17a5c05221e5f0

gcal: Correctly initialise homeostasis parameter
Fixes a regression introduced in 2a17d01be8a665fdb8f6608337a73eaa2c032bad.

sim/gcal.cpp
@@ -121,7 +121,7 @@ public:
 
         // Cortex Sheet (V1)
         CX.svgpath = root.get("CX_svgpath", "boundaries/trialmod.svg").asString();
-        CX.init({beta = beta, lambda = lambda, mu = mu, thetaInit = thetaInit});
+        CX.init({.beta = beta, .mu = mu, .lambda = lambda, .thetaInit = thetaInit});
         CX.allocate();
 
         // afferent projection from ON/OFF cells